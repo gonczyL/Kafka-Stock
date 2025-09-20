62131a7dfe4e74201f96e9ba0fcdae993d8c6b63
GonczyL <gonczykeee@gmail.com>
2025-09-20 13:54:52 +0200

    base

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os, requests, datetime as dt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("FINNHUB_TOKEN")
+STOCK = "AAPL"
+
 def main():
-    print("hey there")
+
+    q = requests.get(
+        "https://finnhub.io/api/v1/quote",
+        params={"symbol":STOCK,"token":TOKEN}
+    ).json()
+    print("AAPL snapshot:", q)  # keys: c= current, h/l/o/p, t=unix
+    ts = q["t"]
+    print("As datetime (UTC):", dt.datetime.utcfromtimestamp(ts))
+
 
 if __name__=="__main__":
     main()
